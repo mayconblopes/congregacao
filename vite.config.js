dff769b6bb69b68a9383667734a522bf189d8e71
Maycon Lopes <lopes.maycon@gmail.com>
2023-04-08 19:03:16 -0300

    preparing for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/congregacao/',
+  // base: '/congregacao/',
   define: {
     'process.env': process.env
   },
